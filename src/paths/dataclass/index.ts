f77da5ec24576b62e9ce84a6925d9b13bfa43aa6
Mathieu Savy <mathieu.savy@gmail.com>
2016-06-29 11:05:10 +0200

    Refactor dataClass method path to separated file

--- src/paths/dataclass/index.ts
+++ src/paths/dataclass/index.ts
@@ -3,6 +3,7 @@ import {HTTPVerb} from '../../http-verb';
 import {IWakandaDataClass} from '../../';
 import {collectionEndpoint} from './collection';
 import {createEndpoint} from './create';
+import {methodPath} from './method-path';
 
 export function dataClassPaths(dataClass: IWakandaDataClass): Path[] {
   const paths: Path[] = [];
@@ -17,28 +18,7 @@ export function dataClassPaths(dataClass: IWakandaDataClass): Path[] {
   dataClass.methods
     .filter(x => x.applyTo === 'dataClass')
     .forEach(method => {
-      const methodPath = new Path(`/${dataClass.name}/_method/${method.name}`);
-      const methodEndpoint = new Endpoint();
-
-      methodEndpoint.description = `Call the \`${method.name}\` method on ${dataClass.name} dataClass.`;
-      methodEndpoint.parameters.push(new Parameter({
-        name: 'body',
-        in: 'body',
-        schema: {
-          type: 'object'
-        }
-      }));
-      methodEndpoint.responses.push(new Response({
-        id: '200',
-        description: 'Successful response',
-        schema: {
-          type: 'object'
-        }
-      }));
-      methodEndpoint.tags.push(dataClass.name, 'DataClass Method');
-
-      methodPath.addEndpoint(HTTPVerb.POST, methodEndpoint);
-      paths.push(methodPath);
+      paths.push(methodPath(method, dataClass));
     });
 
   return paths;
---
+++ src/paths/dataclass/method-path.ts
@@ -0,0 +1,29 @@
+import {Endpoint, Parameter, Path, Response} from '../../swagger/path';
+import {HTTPVerb} from '../../http-verb';
+import {IWakandaDataClass, IWakandaMethod} from '../../';
+
+export function methodPath(method: IWakandaMethod, dataClass: IWakandaDataClass): Path {
+  const methodPath = new Path(`/${dataClass.name}/_method/${method.name}`);
+  const methodEndpoint = new Endpoint();
+
+  methodEndpoint.description = `Call the \`${method.name}\` method on ${dataClass.name} dataClass.`;
+  methodEndpoint.parameters.push(new Parameter({
+    name: 'body',
+    in: 'body',
+    schema: {
+      type: 'object'
+    }
+  }));
+  methodEndpoint.responses.push(new Response({
+    id: '200',
+    description: 'Successful response',
+    schema: {
+      type: 'object'
+    }
+  }));
+  methodEndpoint.tags.push(dataClass.name, 'DataClass Method');
+
+  methodPath.addEndpoint(HTTPVerb.POST, methodEndpoint);
+
+  return methodPath;
+}
