d8bff61d0d73f8d3393a2e181ea2df84946ea8b0
Mathieu Savy <mathieu.savy@gmail.com>
2016-06-29 10:30:15 +0200

    Add dataClass method paths to document

--- src/paths/dataclass/index.ts
+++ src/paths/dataclass/index.ts
@@ -14,7 +14,32 @@ export function dataClassPaths(dataClass: IWakandaDataClass): Path[] {
 
   paths.push(basicPath);
 
-  //TODO - Add a path for each dataClass method
+  dataClass.methods
+    .filter(x => x.applyTo === 'dataClass')
+    .forEach(method => {
+      const methodPath = new Path(`/${dataClass.name}/_method/${method.name}`);
+      const methodEndpoint = new Endpoint();
+
+      methodEndpoint.description = `Call the \`${method.name}\` method on ${dataClass.name} dataClass.`;
+      methodEndpoint.parameters.push(new Parameter({
+        name: 'body',
+        in: 'body',
+        schema: {
+          type: 'object'
+        }
+      }));
+      methodEndpoint.responses.push(new Response({
+        id: '200',
+        description: 'Successful response',
+        schema: {
+          type: 'object'
+        }
+      }));
+      methodEndpoint.tags.push(dataClass.name, 'DataClass Method');
+
+      methodPath.addEndpoint(HTTPVerb.POST, methodEndpoint);
+      paths.push(methodPath);
+    });
 
   return paths;
 }
